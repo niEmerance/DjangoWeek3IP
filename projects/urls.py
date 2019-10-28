f2aced1cf51f527a82e49e43d5e759dad4ca2e1e
niEmerance <niyemera6@gmail.com>
2019-10-28 10:16:13 +0200

    implement login

--- projects/urls.py
+++ projects/urls.py
@@ -7,5 +7,5 @@ app_name='projects'
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'register/',views.register,name = 'register'),
-    
+    url(r'login/',views.login_view,name='login'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,logout
 from django.shortcuts import render,redirect
 
 # Create your views here.
@@ -17,3 +19,13 @@ def register(response):
         form = RegisterForm()
     return render(response, "registration/register.html", {"form":form})
 
+def login_view(request):
+    if request.method=="POST":
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user=form.get_user()
+            login(request,user)
+            return redirect('projects:welcome')
+    else:
+        form=AuthenticationForm()
+    return render(request, 'registration/login.html',{"form":form})
