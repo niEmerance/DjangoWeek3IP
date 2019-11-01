d7e9140f9f390963896c6e19e53e54742cdb1bcc
niEmerance <niyemera6@gmail.com>
2019-11-01 12:36:29 +0200

    some tests

--- projects/models.py
+++ projects/models.py
@@ -13,6 +13,9 @@ class Profile(models.Model):
     def __str__(self):
         return self.bio
 
+    def save_profile(self):
+        self.save()
+
 class Project(models.Model):
     prj_title = models.CharField(max_length =30)
     prj_image = models.ImageField(upload_to = 'photos/')
--- projects/tests.py
+++ projects/tests.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-
+from .models import Profile,Project,Comment
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Profile(profile_pic = 'default.jpg', bio ='Muriuki', contact ='0785236547')
+        self.james.save_profile()
+
+class ProjectTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.pitch= Project(prj_image= 'default.jpg', prj_title ='Muriuki', prj_link ='www.emy.com')
+        self.pitch.save_project()
